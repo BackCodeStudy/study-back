bfd140ae75140b901e1722b105154753e0a9b77e
taewoocode <taewoocode@gmail.com>
2025-04-02 02:16:14 +0900

    BAC-5 [feat]: 프로덕트 레포지토리 추가

--- src/main/java/com/auction/backcodestudy/product/repository/ProductRepository.java
+++ src/main/java/com/auction/backcodestudy/product/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.auction.backcodestudy.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.auction.backcodestudy.product.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
