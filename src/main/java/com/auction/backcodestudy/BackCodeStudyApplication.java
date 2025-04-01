cdc4ef98934f79510e500802a78cd4b78a81b462
김민주 <tptlffldk97@naver.com>
2025-04-01 16:44:45 +0900

    first commit

---
+++ src/main/java/com/auction/backcodestudy/BackCodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.auction.backcodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCodeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCodeStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auction/backcodestudy/BackCodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.backcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCodeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
