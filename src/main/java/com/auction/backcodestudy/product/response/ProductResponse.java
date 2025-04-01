f5889132609e3d61f91bdc870f0ddac6fc7ba031
taewoocode <taewoocode@gmail.com>
2025-04-02 03:46:38 +0900

    BAC-6 [feat]: 프로덕트 반환값 추가

--- src/main/java/com/auction/backcodestudy/product/response/ProductResponse.java
+++ src/main/java/com/auction/backcodestudy/product/response/ProductResponse.java
@@ -15,6 +15,8 @@ public class ProductResponse {
 	private BigDecimal startingPrice;
 	private LocalDateTime localDateTime;
 	private String winnerName;
+	private String status;
+	private LocalDateTime createdAt;
 
 	public ProductResponse(Product product) {
 		this.id = product.getId();
@@ -23,5 +25,11 @@ public class ProductResponse {
 		this.startingPrice = product.getStartingPrice();
 		this.localDateTime = product.getLocalDateTime();
 		this.winnerName = product.getWinner() != null ? product.getWinner().getUsername() : null;
+		this.status = calculateStatus(product);
+		this.createdAt = getCreatedAt();
+	}
+
+	private String calculateStatus(Product product) {
+		return product.getLocalDateTime().isBefore(LocalDateTime.now()) ? "Finished" : "Ongoing";
 	}
 }
