1fc2eb6d11f47c7983f2eba2d41e2bcbfbd7649c
taewoocode <taewoocode@gmail.com>
2025-04-01 19:43:40 +0900

    BAC-5 [feat]: ProductResponse 추가

---
+++ src/main/java/com/auction/backcodestudy/product/response/ProductResponse.java
@@ -0,0 +1,27 @@
+package com.auction.backcodestudy.product.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.auction.backcodestudy.product.entity.Product;
+
+import lombok.Data;
+
+@Data
+public class ProductResponse {
+	private Long id;
+	private String name;
+	private String description;
+	private BigDecimal startingPrice;
+	private LocalDateTime localDateTime;
+	private String winnerName;
+
+	public ProductResponse(Product product) {
+		this.id = product.getId();
+		this.name = product.getName();
+		this.description = product.getDescription();
+		this.startingPrice = product.getStartingPrice();
+		this.localDateTime = product.getLocalDateTime();
+		this.winnerName = product.getWinner() != null ? product.getWinner().getUsername() : null;
+	}
+}
